a108dd51c6ef095002f75635bd0fd54a2b00c2ef
dylanxhernandez <dylan.x.h.developer@gmail.com>
2023-09-10 00:29:38 -0400

    feat: add new routes and components for each major financial function

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,16 @@ function App() {
   return (
     <div className="App">
       <h1>Open Financial Calculator</h1>
-      <div>
+      <div className="strong">
         <a href="/">Home</a>&nbsp;&bull;&nbsp;
         <a href="/ror">Rate of Return</a>&nbsp;&bull;&nbsp;
-        <a href="/fv">Future Value</a>
+        <a href="/fv">Future Value</a>&nbsp;&bull;&nbsp;
+        <a href="/fa">Future Amount</a>&nbsp;&bull;&nbsp;
+        <a href="/cd">Capital Depletion</a>&nbsp;&bull;&nbsp;
+        <a href="/mp">Major Purchase</a>&nbsp;&bull;&nbsp;
+        <a href="/di">Desired Income</a>
       </div>
+      <hr/>
       <AppRouter />
     </div>
   );
--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -5,6 +5,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import RateOfReturn from "./components/RateOfReturn";
 import FutureValue from "./components/FutureValue";
+import FutureAmount from "./components/FutureAmount";
+import CapitalDepletion from "./components/CapitalDepletion";
+import MajorPurchase from "./components/MajorPurchase";
+import DesiredIncome from "./components/DesiredIncome";
 
 function AppRouter() {
   return (
@@ -13,6 +17,10 @@ function AppRouter() {
         <Route path="/" element={<Home />} />
         <Route path="/ror" element={<RateOfReturn />} />
         <Route path="/fv" element={<FutureValue />} />
+        <Route path="/fa" element={<FutureAmount />} />
+        <Route path="/cd" element={<CapitalDepletion />} />
+        <Route path="/mp" element={<MajorPurchase />} />
+        <Route path="/di" element={<DesiredIncome />} />
       </Routes>
     </BrowserRouter>
   );
